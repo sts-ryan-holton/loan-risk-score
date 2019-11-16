9f90634ffaa728e72f925b1ec353756e33c3bc9b
Ryan <ryan.holton@stsonline.uk.com>
2019-11-16 14:45:07 +0000

    :rocket: Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# :moneybag: Loan Affordability (Risk Score)
+
+This project uses machine learning [neural networks](https://en.wikipedia.org/wiki/Artificial_neural_network) using [Brain JS](https://github.com/BrainJS/brain.js#brainjs) to calculate an applicant's likelihood of being able to repay a loan. A **Risk Score** is provided to the applicant ranging from `0` to `500`, the higher the number, the better (more chance of affording the loan)
+
+
+## :star2: Demo
+
+Coming soon...
+
+
+## :bulb: How it works
+
+**We use [Brain JS](https://github.com/BrainJS/brain.js#brainjs)**
+
+The core concept of this system is to provide a **score** to the user on their likelihood of being able to repay a loan using machine learning. This works by feeding the machine learning system a set of "training data" in a JSON format, providing an input, and an output as to whether the input is good or bad, the system can then figure out a probability between 0-100 _(100 being very likely to be able to afford a loan)_
+
+
+## :books: Training Data
+
+Training data collected from hundreds of jobs via Node JS scraping using Cheerio and Puppeteer _(development use only)_
+
+Using the below assumptions, if a take-home pay is provided lower than **£1,500** a month, and doesn't look accurate, a lower probability will occur.
+
+#### Salary
+
+1. If min & max salary provided, they're added together and divided to get a yearly average.
+2. If single salary provided, no extra assumptions made.
+3. If hourly salary provided, we randomly pick either 37 or 25 hrs a week, and 4.3 weeks a month, calculated over a year.
+4. All salaries are then divided by 12 to get a monthly salary.
+5. All salaries have approx 17% deducted as a buffer for tax and NI.
+
+#### Expenses
+
+1. £190/month, **Utility Bills (Gas, Electric, Water, Phone, TV, Broadband)**, [source](https://www.moneyadviceservice.org.uk/blog/what-is-the-average-cost-of-utility-bills-per-month)
+2. £600/month, **Rent/Mortgage (1 bedroom)**, [source](https://www.bbc.co.uk/news/business-46072509)
+3. £260/month, **Food shopping (small family)**, [source](https://www.moneyadviceservice.org.uk/blog/how-does-your-household-food-spend-compare)
+
+#### Loan
+
+1. A loan average of £500 taken based on approx 6,000 applicants.
+
+
+## :mag: Scraper
+
+Install the scraper, you need **Node JS 10 or greater**
+
+``` bash
+# clone project
+$ git clone git@github.com:team8digital/llcV2.git
+
+# install dependencies.
+$ npm install
+```
+
+### Starting
+
+``` bash
+# start scraping
+$ node scraper/scrape-jobs.js
+```
+
+### Starting - With options
+
+``` bash
+# start scraping with options (you can pass all, or some of the following with number format)
+$ node scraper/scrape-jobs.js --searchRadius=300 --memThreshold=50 --tempThreshold=93 --scrapeInterval=1000 --thresholdDelay=7500
+```
---
+++ scraper/scrape-jobs.js
@@ -0,0 +1,180 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio')
+const fs = require('fs')
+const si = require('systeminformation');
+const args = require('minimist')(process.argv.slice(2))
+const writeStream = fs.createWriteStream('./scraper/data/salary.json')
+
+
+// define global vars
+var searchRadius   = (args['searchRadius'] && args['searchRadius'] != '') ? parseInt(args['searchRadius']) : 300,
+    pageNumber     = 00,
+    scrapedJobs    = 0,
+    unableToScrape = 0,
+    cpuTemp        = 0,
+    memThreshold   = (args['memThreshold'] && args['memThreshold'] != '') ? parseInt(args['memThreshold']) : 60,
+    tempThreshold  = (args['tempThreshold'] && args['tempThreshold'] != '') ? parseInt(args['tempThreshold']) : 93,
+    scrapeInterval = (args['scrapeInterval'] && args['scrapeInterval'] != '') ? parseInt(args['scrapeInterval']) : 1000,
+    pauseDelay     = (args['thresholdDelay'] && args['thresholdDelay'] != '') ? parseInt(args['thresholdDelay']) : 7500,
+    pauseScraping  = false,
+    scrapeOnRun    = true,
+    urlToScrape    = 'https://www.indeed.co.uk/jobs?l=Bridgend',
+    jobCard        = '.jobsearch-SerpJobCard',
+    jobCardSalary  = '.salarySnippet .salaryText'
+
+
+// scrape the website
+async function scrapeWebsite(radius, pageNo) {
+
+  // calculate CPU temperature
+  si.cpuTemperature()
+    .then(data => cpuTemp = data.main)
+    .catch(error => console.error(error));
+
+  // get memory usage of scraper
+  const used = process.memoryUsage().heapUsed / (1024 * 1024);
+
+  // pause scraper if Memory or Temp exceeds defined values
+  if (used > memThreshold || cpuTemp > tempThreshold) {
+    console.log('== SCRAPING PAUSED ===')
+    pauseScraping = true
+    setTimeout(function(){
+      console.log('== SCRAPING RESUMED ===')
+      pauseScraping = false
+    }, pauseDelay);
+  }
+
+  // output stats to terminal
+  console.log(`=== SCRAPING: PAGE: ${pageNumber}, JOBS: ${scrapedJobs}, ERRORED: ${unableToScrape}, MEM: ${used}, TEMP: ${cpuTemp} ===`)
+
+  // init Puppeteer
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 926 });
+  await page.goto(`${urlToScrape}&radius=${radius}&start=${pageNo}`);
+
+  // get all HTML content from fetched URL
+  let pageData = await page.evaluate(() => document.body.innerHTML);
+
+  // define Cheerio to use jQuery like syntax
+  const $ = await cheerio.load(pageData)
+
+  // get job cards
+  $(jobCard).each((index, job) => {
+    if ($(job).find(jobCardSalary).length) {
+      const salaryText = $(job).find(jobCardSalary).text()
+
+      // calculate percentages
+      function percentage(percent, percentageFrom) {
+         return (percent / 100) * parseInt(percentageFrom);
+      }
+
+      // salary to add
+      let salary = 0
+
+      if (salaryText.includes('year') || salaryText.includes('annum')) {
+        if (salaryText.includes('-')) {
+          const doubleSalary = salaryText.split('-')
+          var minSalary = doubleSalary[0].replace(/\s/g,'').replace(/,/g, '') // remove whitespace, replace commas
+          var maxSalary = doubleSalary[1].replace(/\s/g,'').replace(/,/g, '') // remove whitespace, replace commas
+          minSalary = minSalary.replace(/\D/g,'') // extract numbers
+          maxSalary = maxSalary.replace(/\D/g,'') // extract numbers
+
+          var aveSalary = (parseInt(minSalary) + parseInt(maxSalary)) / 2 // combine min & max salary, and get an average
+
+          // deduct 17% for tax / NI margin for year
+          aveSalary = parseInt(aveSalary) - percentage(17, parseInt(aveSalary))
+
+          // salary output: average doubled salary, monthly
+          salary = parseInt(aveSalary) / 12
+
+        } else {
+          const singleSalary = salaryText.replace(/\s/g,'').replace(/,/g, '') // remove whitespace, replace commas
+          var singleSalaryRaw = singleSalary.replace(/\D/g,'') // extract numbers
+
+          // deduct 17% for tax / NI margin for year
+          var singleSalaryOutput = parseInt(singleSalary) - percentage(17, parseInt(singleSalaryRaw))
+
+          // salary output: single salary, monthly
+          salary = parseInt(singleSalaryOutput) / 12
+
+        }
+      } else if (salaryText.includes('hour')) {
+        const hourlySalary = salaryText
+            .replace(/[^0-9.-]/g, '') // remove chars except number, hyphen, point.
+            .replace(/(\..*)\./g, '$1')  // remove multiple points.
+            .replace(/(?!^)-/g, '') // remove middle hyphen.
+            .replace(/^0+(\d)/gm, '$1') // remove multiple leading zeros.
+
+        var randomNumber = Math.floor(Math.random() * 10),
+            salaryHours = (randomNumber < 5) ? 37 : 25,
+            salaryWeeks = 4.3,
+            salaryMonths = 12
+
+        if (hourlySalary.length < 6) {
+          var aveYearFromHour = Math.round(parseInt(hourlySalary) * salaryHours * salaryWeeks * salaryMonths) // weekly hours X weeks in month X months in year
+
+          // deduct 17% for tax / NI margin for year
+          aveYearFromHour = aveYearFromHour - percentage(17, parseInt(aveYearFromHour))
+
+          // salary output: single salary, monthly
+          salary = parseInt(aveYearFromHour) / 12
+        }
+
+      }
+
+      // determine affordability
+      var machineLearningOutput = (parseInt(salary) < 1500) ? 0 : 1
+
+      // add starting zeros
+      function leftFillNum(num, targetLength) {
+        return num.toString().padStart(targetLength, 0);
+      }
+
+      // check if salary is a 3 digit number and add starting zero to make 4 digits
+      if (salary > 0 && salary < 1000) {
+        salary = leftFillNum(Math.trunc(salary), 4)
+      } else {
+        salary = Math.trunc(salary)
+      }
+
+      // create compatible format for machine learning
+      salary = salary.toString().split('').join(',')
+
+      // build JSON file for data consumption
+      if (!isNaN(parseInt(salary)) && salary.length > 2 && salary.length < 8) {
+        writeStream.write(`{input:[${salary}],output:[${machineLearningOutput}]},`)
+        scrapedJobs++
+      } else {
+        unableToScrape++
+      }
+
+      // close browser after completion to prevent memory overloading
+      browser.close();
+
+    }
+  });
+
+  // increment page number to pick new page
+  if (pageNumber < 50000) {
+    pageNumber+=10
+  } else {
+    pageNumber = 0
+  }
+}
+
+
+// run the scraper on loop and increment details
+if (scrapeOnRun) {
+  setInterval(() => {
+    if (!pauseScraping) {
+      scrapeWebsite(searchRadius, pageNumber)
+    }
+  }, scrapeInterval);
+}
+
+
+// run the scraper on start
+if (scrapeOnRun) {
+  scrapeWebsite(searchRadius, pageNumber)
+}
